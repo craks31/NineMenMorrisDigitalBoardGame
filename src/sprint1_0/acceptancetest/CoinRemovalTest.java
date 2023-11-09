689d71905a7663418f6f0282653ad830854ca303
craks31 <prgcf@umkc.edu>
2023-11-09 17:05:00 -0600

    updated remove tests

--- src/sprint1_0/acceptancetest/CoinRemovalTest.java
+++ src/sprint1_0/acceptancetest/CoinRemovalTest.java
@@ -12,8 +12,6 @@ import org.testfx.framework.junit.ApplicationTest;
 
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import sprint1_0.product.constants.GameConstants;
 import sprint1_0.product.model.Position;
@@ -130,93 +128,11 @@ public class CoinRemovalTest extends ApplicationTest {
     PositionCircle player1PositionCircle3 = from(rootNode).lookup("#position3").query();
     clickOn(player1PositionCircle3);
     // remove
-    for (Position p :
-        gameManagerGUI.getBoard().getAllPositionList().stream()
-            .filter(pos -> !pos.isFilled())
-            .collect(Collectors.toList())) {
-      assertEquals(true, p.getPositionCircle().isDisabled());
+    for(Position p : gameManagerGUI.getBoard().getAllPositionList().stream()
+            .filter(pos -> !pos.isFilled()).collect(Collectors.toList())){
+    	assertEquals(true, p.getPositionCircle().isDisabled());
     }
   }
 
-  @Test
-  public void movingBacktoFillPositionAfterRemoval() {
-    // 1,2,3 is the vertical Mill
-    PositionCircle player1PositionCircle1 = from(rootNode).lookup("#position1").query();
-    clickOn(player1PositionCircle1);
-    PositionCircle player2PositionCircle1 = from(rootNode).lookup("#position4").query();
-    clickOn(player2PositionCircle1);
-    PositionCircle player1PositionCircle2 = from(rootNode).lookup("#position2").query();
-    clickOn(player1PositionCircle2);
-    PositionCircle player2PositionCircle2 = from(rootNode).lookup("#position6").query();
-    clickOn(player2PositionCircle2);
-    PositionCircle player1PositionCircle3 = from(rootNode).lookup("#position3").query();
-    clickOn(player1PositionCircle3);
-    // remove
-    clickOn(player2PositionCircle2);
-    assertEquals(GameConstants.FILL, gameManagerGUI.getBoard().getOp());
-  }
-
-  @Test
-  public void movingBcktoMovePositionAfterRemoval() {
-    // 1,2,3 is the vertical Mill
-    int i = 0;
-    gameManagerGUI.getBoard().setMillCheckByPassed(true);
-    while ((!gameManagerGUI.getBoard().getPlayer2().getCoins().isEmpty())
-        || (!gameManagerGUI.getBoard().getPlayer1().getCoins().isEmpty())) {
-      String pos = "#position" + String.valueOf(i + 1);
-      PositionCircle c = from(rootNode).lookup(pos).query();
-      clickOn(c);
-      i++;
-    }
-    gameManagerGUI.getBoard().setMillCheckByPassed(false);
-    Color displayColor =
-        (Color) ((Circle) from(rootNode).lookup("#displayCircle").query()).getFill();
-    while (!gameManagerGUI.getBoard().getOp().equals(GameConstants.REMOVE)) {
-      if (displayColor.equals(GameConstants.PLAYER1COLOR)) {
-        Position position =
-            gameManagerGUI.getBoard().getPlayer1().getFilledPositions().stream().findFirst().get();
-        moveFunction(position, displayColor);
-      } else if (displayColor.equals(GameConstants.PLAYER2COLOR)) {
-        Position position =
-            gameManagerGUI.getBoard().getPlayer2().getFilledPositions().stream().findFirst().get();
-        moveFunction(position, displayColor);
-      }
-    }
-    System.out.println(gameManagerGUI.getBoard().getOp());
-    // clickOn(player2PositionCircle2);
-    assertEquals(GameConstants.REMOVE, gameManagerGUI.getBoard().getOp());
-  }
-
-  private void moveFunction(Position position, Color displayColor) {
-    String pos = "#" + String.valueOf(position.getPositionCircle().getId());
-    PositionCircle positionCircle = from(rootNode).lookup(pos).query();
-    gameManagerGUI.getBoard().setOp(GameConstants.MOVE);
-    clickOn(positionCircle);
-
-    if (position.getUp() != null && !position.getUp().isFilled()) {
-      String posup = "#" + String.valueOf(position.getUp().getPositionCircle().getId());
-      PositionCircle positionCircleup = from(rootNode).lookup(posup).query();
-      clickOn(positionCircleup);
-      assertEquals(displayColor, positionCircleup.getFill());
-      assertEquals(GameConstants.BACKGROUNDCOLOR, positionCircle.getFill());
-    } else if (position.getLeft() != null && !position.getLeft().isFilled()) {
-      String posLeft = "#" + String.valueOf(position.getUp().getPositionCircle().getId());
-      PositionCircle positionCircleLeft = from(rootNode).lookup(posLeft).query();
-      clickOn(positionCircleLeft);
-      assertEquals(displayColor, positionCircleLeft.getFill());
-      assertEquals(GameConstants.BACKGROUNDCOLOR, positionCircle.getFill());
-    } else if (position.getRight() != null && !position.getRight().isFilled()) {
-      String posRight = "#" + String.valueOf(position.getUp().getPositionCircle().getId());
-      PositionCircle positionCircleRight = from(rootNode).lookup(posRight).query();
-      clickOn(positionCircleRight);
-      assertEquals(displayColor, positionCircleRight.getFill());
-      assertEquals(GameConstants.BACKGROUNDCOLOR, positionCircle.getFill());
-    } else if (position.getDown() != null && !position.getDown().isFilled()) {
-      String posDown = "#" + String.valueOf(position.getUp().getPositionCircle().getId());
-      PositionCircle positionCircleDown = from(rootNode).lookup(posDown).query();
-      clickOn(positionCircleDown);
-      assertEquals(displayColor, positionCircleDown.getFill());
-      assertEquals(GameConstants.BACKGROUNDCOLOR, positionCircle.getFill());
-    }
-  }
+  
 }
